9b1e914408bdab5dfad53bc3f9fd70f9af995bc5
“PonceGL” <poncianogl@hotmail.com>
2024-06-09 13:18:40 -0600

    fix: remove auto signing

--- signing-assets/build-mac.sh
+++ signing-assets/build-mac.sh
@@ -1,8 +1,10 @@
 # https://www.electron.build/code-signing.html
 # https://www.youtube.com/watch?v=WnipZaYslRc
 
-source ".env"
-export CSC_LINK=$CERTIFICATE_PATH
-export CSC_KEY_PASSWORD=$CERTIFICATE_PASSWORD
-export CSC_NAME=$CERTIFICATE_NAME
-export CSC_KEYCHAIN=$CERTIFICATE_KEYCHAIN
+# source ".env"
+# export CSC_LINK=$CERTIFICATE_PATH
+# export CSC_KEY_PASSWORD=$CERTIFICATE_PASSWORD
+# export CSC_NAME=$CERTIFICATE_NAME
+# export CSC_KEYCHAIN=$CERTIFICATE_KEYCHAIN
+
+export CSC_IDENTITY_AUTO_DISCOVERY=false
